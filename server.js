ca87e9a643e8d87e92dccefd0d0208f074a329eb
ankita shukla <ankita.shukla8287@gmail.com>
2023-09-27 01:04:41 +0530

    fixed path

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use("/", contactRoute);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client"));
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client","public", "index.html"))
   );
 }
 
